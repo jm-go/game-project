5381c4403e304174bac53f3c02827be708dc9b7b
Jowita Gogol <jmg88@bath.ac.uk>
2023-12-04 22:19:14 +0000

    Added array with words list

---
+++ src/data.ts
@@ -0,0 +1,37 @@
+export type Words = {
+  word: string;
+  category: string;
+};
+
+export const wordsArray: Words[] = [
+  { word: "ocean", category: "Nature" },
+  { word: "puzzle", category: "Objects" },
+  { word: "guitar", category: "Music" },
+  { word: "jungle", category: "Nature" },
+  { word: "castle", category: "Architecture" },
+  { word: "wizard", category: "Fantasy" },
+  { word: "ballet", category: "Dance" },
+  { word: "laptop", category: "Technology" },
+  { word: "volcano", category: "Nature" },
+  { word: "glacier", category: "Nature" },
+  { word: "kitten", category: "Animals" },
+  { word: "museum", category: "Places" },
+  { word: "recipe", category: "Food" },
+  { word: "comedy", category: "Entertainment" },
+  { word: "pirate", category: "Fantasy" },
+  { word: "sphinx", category: "Mythology" },
+  { word: "jacket", category: "Clothing" },
+  { word: "gadget", category: "Technology" },
+  { word: "fossil", category: "Science" },
+  { word: "harvest", category: "Agriculture" },
+  { word: "metropolitan", category: "Places" },
+  { word: "archaeology", category: "Science" },
+  { word: "conundrum", category: "Concepts" },
+  { word: "philosophy", category: "Academics" },
+  { word: "biotechnology", category: "Science" },
+  { word: "antioxidant", category: "Health" },
+  { word: "cartography", category: "Art" },
+  { word: "microscope", category: "Science" },
+  { word: "revolutionary", category: "History" },
+  { word: "sustainability", category: "Environment" },
+];
